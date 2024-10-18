9b7e6c91d6a03a4ed4f8bb9bdce3e62e2bed9580
LunerBreeze <RainSoul1120@gmail.com>
2024-10-18 17:36:36 +0800

    使用Javassist生成DaoImpl类

---
+++ 004-mybatis-web/src/test/java/com/lb/test/JavassistTest.java
@@ -0,0 +1,32 @@
+
+package com.lb.test;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.Modifier;
+
+import java.lang.reflect.Method;
+
+public class JavassistTest {
+    public static void main(String[] args) throws Exception {
+        // 获取类池
+        ClassPool pool = ClassPool.getDefault();
+        // 创建类
+        CtClass ctClass = pool.makeClass("com.powernode.javassist.Test");
+        // 创建方法
+        // 1.返回值类型 2.方法名 3.形式参数列表 4.所属类
+        CtMethod ctMethod = new CtMethod(CtClass.voidType, "execute", new CtClass[]{}, ctClass);
+        // 设置方法的修饰符列表
+        ctMethod.setModifiers(Modifier.PUBLIC);
+        // 设置方法体
+        ctMethod.setBody("{System.out.println(\"hello world\");}");
+        // 给类添加方法
+        ctClass.addMethod(ctMethod);
+        // 调用方法
+        Class<?> aClass = ctClass.toClass();
+        Object o = aClass.newInstance();
+        Method method = aClass.getDeclaredMethod("execute");
+        method.invoke(o);
+    }
+}
